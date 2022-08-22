ce3349500df89617042b92a31819530e935fa50e
Victorious <new@mail.com>
2022-08-22 14:24:05 +0300

    commit tree 1b66ce45724935ba4fd4af68434d7fe5543c6cff

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(e) {
+    console.log(e)
+})
+document.getElementById("text")
+e.text.innerHTML = "This is really cool"
